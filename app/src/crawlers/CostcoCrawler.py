9739e6e2f1a8634e96fd94cb431984e49b3b5fc3
Yusheng Ding <davidthinkleding@Gmail.com>
2019-06-24 18:28:27 -0700

    go sample deal

--- app/src/crawlers/CostcoCrawler.py
+++ app/src/crawlers/CostcoCrawler.py
@@ -6,36 +6,48 @@ class CostcoCrawler(BrowserRobot):
     def __init__(self):
         self.driver = None
         BrowserRobot.__init__(self)
-        # self.driver = super().driver
-        pass
 
     def run(self):
         try:
-            self.try_login_costco()
+            self.login()
+            self.go_sample_deal()
             self.save_source()
         except Exception as e:
             print(str(e))
         else:
             self.logout()
 
-    def try_login_costco(self):
-        print(COSTCO_URL)
-        self.driver.get(COSTCO_URL)
-        self.driver.find_element_by_id("header_sign_in").click()
-        self.driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Sign In'])[2]/following::div[2]").click()
-        self.driver.find_element_by_id("logonId").clear()
-        self.driver.find_element_by_id("logonId").send_keys(COSTCO_USERNAME)
-        self.driver.find_element_by_id("logonPassword").clear()
-        self.driver.find_element_by_id("logonPassword").send_keys(COSTCO_PASSWORD)
-        self.driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Remember Me'])[1]/following::input[1]").click()
-        self.driver.get(COSTCO_DEAL_URL)
-        self.driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Also available with AppleCare+'])[1]/following::img[1]").click()
-        self.driver.find_element_by_link_text("New Apple iMac 21.5\" - Intel Core i5 3.0 GHz - 8GB Memory - 1TB Fusion Drive - 2GB Radeon Pro 560X Graphics").click()
-        self.save_source()
-        print("end Login")
+    def login(self):
+        try:
+            print(COSTCO_URL)
+            self.driver.get(COSTCO_URL)
+            self.driver.find_element_by_id("header_sign_in").click()
+            self.driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Sign In'])[2]/following::div[2]").click()
+            self.driver.find_element_by_id("logonId").clear()
+            self.driver.find_element_by_id("logonId").send_keys(COSTCO_USERNAME)
+            self.driver.find_element_by_id("logonPassword").clear()
+            self.driver.find_element_by_id("logonPassword").send_keys(COSTCO_PASSWORD)
+            self.driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Remember Me'])[1]/following::input[1]").click()
+        except Exception as e:
+            print(str(e))
+        else:
+            print("Login Success")
 
     def logout(self):
-        self.driver.get(COSTCO_URL)
-        self.driver.find_element_by_id("myaccount-d").click()
-        self.driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Preferences'])[1]/following::input[1]").click()
-        print("end Logout")
+        try:
+            self.driver.get(COSTCO_URL)
+            self.driver.find_element_by_id("myaccount-d").click()
+            self.driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Preferences'])[1]/following::input[1]").click()
+            print("end Logout")
+        except Exception as e:
+            print(str(e))
+
+    def go_sample_deal(self):
+        try:
+            self.driver.get(COSTCO_DEAL_URL)
+            self.driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Also available with AppleCare+'])[1]/following::img[1]").click()
+            self.driver.find_element_by_link_text("New Apple iMac 21.5\" - Intel Core i5 3.0 GHz - 8GB Memory - 1TB Fusion Drive - 2GB Radeon Pro 560X Graphics").click()
+        except Exception as e:
+            print(str(e))
+        else:
+            return self.driver.page_source
