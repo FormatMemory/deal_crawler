660ed09202c42903afcba724d66ad5fd3fbd191b
Yusheng Ding <davidthinkleding@Gmail.com>
2019-06-25 23:31:47 -0700

    SavePoint: parse json

--- app/src/crawlers/BestBuyCrawler/BestBuyCrawler.py
+++ app/src/crawlers/BestBuyCrawler/BestBuyCrawler.py
@@ -1,7 +1,8 @@
 from config.secret import BESTBUY_API_TOKEN
 from config.settings import BESTBUY_API_PRODUCTS_URL
 import requests
-from src.utils.SaveSource import save_source_to_file, save_json_to_xml
+import json
+from src.utils.SaveSource import save_source_to_file, save_json_to_xml, save_json_to_csv
 
 class BestBuyCrawler:
     '''
@@ -15,23 +16,30 @@ class BestBuyCrawler:
 # &facet=onSale
 # &pageSize=14
 # &format=json
-    def test(self):
+    def run(self):
         params = {
-            "onSale": "true",
             "apiKey" : BESTBUY_API_TOKEN,
             "sort" : "bestSellingRank.asc",
-            "show" : "accessories.sku,addToCartUrl,bestSellingRank,categoryPath.id,categoryPath.name,color,condition,customerReviewAverage,customerReviewCount,description,details.name,details.value,dollarSavings,features.feature,freeShipping,frequentlyPurchasedWith.sku,image,includedItemList.includedItem,inStoreAvailability,inStoreAvailabilityText,longDescription,manufacturer,mobileUrl,modelNumber,name,onlineAvailability,onlineAvailabilityText,onSale,percentSavings,regularPrice,relatedProducts.sku,salePrice,shortDescription,sku,thumbnailImage,type,upc,url",
-            "pageSize" : "1",
+            "show" : "accessories.sku,bestSellingRank,categoryPath.id,categoryPath.name,customerReviewAverage,customerReviewCount,description,details.name,details.value,dollarSavings,features.feature,freeShipping,image,includedItemList.includedItem,longDescription,manufacturer,mobileUrl,modelNumber,name,onSale,percentSavings,regularPrice,relatedProducts.sku,salePrice,shipping,shortDescription,sku,thumbnailImage,type,upc,url",
+            "pageSize" : "3",
+            "page" : "1",
             "acet" : "onSale",
             "format" : "json"
         }
-        res = requests.get(BESTBUY_API_PRODUCTS_URL, params=params)
-        print(BESTBUY_API_PRODUCTS_URL)
-        print(res.status_code)
-        if res.status_code == 200:
-            print(res.text)
+        special_condition = "(onSale=true&percentSavings>50)"
+        try:
+            res = requests.get(BESTBUY_API_PRODUCTS_URL+special_condition, params=params, timeout=3)
+            res.raise_for_status()
+            res.encoding = 'utf-8'
+            # print(res.text)
             # save_source_to_file(res.text)
-            save_json_to_xml(res.text)
+            # save_json_to_xml(res.json()["products"])
+            # print(res.json()["products"])
+            # save_json_to_csv(json.loads(res.json()["products"]))
+            source = res.json()["product"]
+            print(type(source))
+        except Exception as err:
+            print(f'Error occurred: {err}')
         else:
-            print(res.text)
+            print("Success")
         
--- app/src/utils/SaveSource.py
+++ app/src/utils/SaveSource.py
@@ -1,7 +1,7 @@
 import datetime
 import os
 from config.settings import OUTPUT_FILE_PATH
-from json import loads
+import json
 from dicttoxml import dicttoxml
 
 def save_source_to_file(source, *args, **kwargs):
@@ -14,7 +14,7 @@ def save_source_to_file(source, *args, **kwargs):
         with open(path, 'w') as f:
             f.write(source)
     except Exception as e:
-        print("ERROR: " + str(e))
+        print("ERROR at save_source_to_file: " + str(e))
     else:
         print("Save file at "+path)
 
@@ -26,10 +26,31 @@ def save_json_to_xml(source, *args, **kwargs):
         filename = kwargs["filename"]
     path = os.path.join(OUTPUT_FILE_PATH, filename)
     try:
-        xml = dicttoxml(loads(source), attr_type=False)
+        xml = dicttoxml(json.loads(source), attr_type=False)
         with open(path, 'wb') as f:
             f.write(xml)
     except Exception as e:
-        print("ERROR: " + str(e))
+        print("ERROR at save_json_to_xml : " + str(e))
+    else:
+        print("Save file at "+path)
+
+
+def save_json_to_csv(source, *args, **kwargs):
+    print(type(source))
+    return 
+    if not "filename" in kwargs or not kwargs["filename"]:
+        filename = 'test-{date:%Y-%m-%d_%H:%M:%S}.csv'.format( date=datetime.datetime.now() )
+    else:
+        filename = kwargs["filename"]
+    path = os.path.join(OUTPUT_FILE_PATH, filename)
+    try:
+        # data = [json.loads(i) for i in source if i]
+        data = source
+        with open(path, 'wb') as f:
+            f.writerow(data[0].keys())
+            for row in data:
+                f.writerow(row.values())
+    except Exception as e:
+        print("ERROR at save_json_to_csv: " + str(e))
     else:
         print("Save file at "+path)
