6381bf3e2dc1df7821ed3c87825311ec07109e2e
Yusheng Ding <davidthinkleding@Gmail.com>
2019-06-25 18:48:37 -0700

    add save json to xml

--- app/src/utils/SaveSource.py
+++ app/src/utils/SaveSource.py
@@ -1,6 +1,8 @@
 import datetime
 import os
 from config.settings import OUTPUT_FILE_PATH
+from json import loads
+from dicttoxml import dicttoxml
 
 def save_source_to_file(source, *args, **kwargs):
     if not "filename" in kwargs or not kwargs["filename"]:
@@ -8,10 +10,25 @@ def save_source_to_file(source, *args, **kwargs):
     else:
         filename = kwargs["filename"]
     path = os.path.join(OUTPUT_FILE_PATH, filename)
-    html = source
     try:
         with open(path, 'w') as f:
-            f.write(html)
+            f.write(source)
+    except Exception as e:
+        print("ERROR: " + str(e))
+    else:
+        print("Save file at "+path)
+
+
+def save_json_to_xml(source, *args, **kwargs):
+    if not "filename" in kwargs or not kwargs["filename"]:
+        filename = 'test-{date:%Y-%m-%d_%H:%M:%S}.xml'.format( date=datetime.datetime.now() )
+    else:
+        filename = kwargs["filename"]
+    path = os.path.join(OUTPUT_FILE_PATH, filename)
+    try:
+        xml = dicttoxml(loads(source), attr_type=False)
+        with open(path, 'wb') as f:
+            f.write(xml)
     except Exception as e:
         print("ERROR: " + str(e))
     else:
