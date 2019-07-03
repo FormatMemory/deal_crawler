a29601a095ec3e33d29eaba51162e0be43bd60a7
Yusheng Ding <davidthinkleding@Gmail.com>
2019-07-02 18:39:14 -0700

    delete extra img file

--- app/src/utils/Uploader.py
+++ app/src/utils/Uploader.py
@@ -3,7 +3,9 @@ import time
 from config.settings import IMG_SAVE_PATH, DEBUG_MODE
 from config.secret import DEAL_SITE_API_DOOR, DEAL_SITE_TOKEN, DEAL_SITE_API_ENDPOINT
 from src.utils.NameGenerators import generateUniqueFileName
-
+import json
+from PIL import Image
+import io
 
 class Uploader:
     '''
@@ -16,7 +18,8 @@ class Uploader:
         if not DEAL_SITE_TOKEN:
             raise Exception("Error when initialting Uploader: TOKEN cannot be empty...")
         self.headers = {
-            "Authorization": "Token " + DEAL_SITE_TOKEN
+            "Authorization": "Token " + DEAL_SITE_TOKEN,
+            "content-type": "multipart/form-data"
         }
 
     def upload_deals(self, deals, image_fields = ["image"], deal_chunk=5, upload_link = DEAL_SITE_API_DOOR):
@@ -47,26 +50,35 @@ class Uploader:
             raise Exception("Error orrcused at Uploader.upload_single_deal: input deal should at least contain title, body and image")
         
         images = {}
-        for image_filed in image_fields:
-            image_name, content, file_path = self.image_getter(deal, image_filed)
-            images[image_filed] = (image_name, open(file_path, 'wr'), "multipart/form-data")
-            del deal[image_filed]
+        image_paths = []
+        for image_field in image_fields:
+            image_name, file_path, img_file = self.image_getter(deal, image_field, save_file=False)
+            images[image_field] = (image_name, img_file)
+            # deal[image_field] = image_name
+            del deal[image_field]
             # ('file.zip', open('file.zip', 'rb'), 'text/plain')
-        if DEBUG_MODE:
-            print("Upload:")
-            print(deal)
-            print("\n")
-        
-        res = requests.post(upload_link, files=images, json=deal, headers=self.headers)
-        time.sleep(1)
-        print(res.status_code)
-        if res.status_code == 400:
-            print(res.reason)
-            print(res.text)
-            raise Exception("Error orrcused at Uploader.upload_single_deal: upload error")
+        try:
+            # res = requests.post(upload_link, files=images, data=deal, headers=self.headers)
+            if DEBUG_MODE:
+                print("Upload:")
+                print(json.dumps(deal))
+                print(images)
+                print("\n")
+
+            res = requests.post(upload_link,  params=json.dumps(deal), headers=self.headers)
+            print(res.status_code)
+            if res.status_code == 400:
+                print(res.reason)
+                print(res.text)
+                raise Exception("Error orrcused at Uploader.upload_single_deal: upload error return status 400")
+            
+        except Exception as e:
+            print("Error orrcused at Uploader.upload_single_deal: "+str(e))
+        finally:
+            time.sleep(1)
 
 
-    def image_getter(self, deal, image_field = "image", save_file=True, save_path=IMG_SAVE_PATH):
+    def image_getter(self, deal, image_field = "image", save_file=False, save_path=IMG_SAVE_PATH):
         '''
         Get Image file from link deal[image_field]
         Save file to SAVE_PATH
@@ -85,4 +97,7 @@ class Uploader:
             with open(save_path + name, 'wb') as f:
                 for chunk in r.iter_content(chunk_size = 128):
                     f.write(chunk)
-        return name, r.content,save_path + name 
+            img_file = open(file_path, 'rb')
+        else:
+            img_file = Image.open(io.BytesIO(r.content))
+        return name,  save_path + name, img_file
